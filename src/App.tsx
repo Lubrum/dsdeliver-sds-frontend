eabd1be0704461efe10e8261faafe3eb43e7ff2d
Lubrum <lucianobrum18@gmail.com>
2023-02-12 12:14:34 -0300

    Bring project from dsdeliver-sds  to here.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
+import Routess from './Routess';
+
+
+function App() {
+  return (
+    <>
+      <Routess />
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
